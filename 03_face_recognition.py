a470ac376278a44f6a4a21bae401f9d97f597c34
alvianavivi <alvianagithub@gmail.com>
2023-04-27 14:15:45 +0700

    02

--- 03_face_recognition.py
+++ 03_face_recognition.py
@@ -15,7 +15,7 @@ import os
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + cascadePath)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
