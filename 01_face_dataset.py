fb2028699b52b38077d60a4385fe153b1fe28f73
alvianavivi <alvianagithub@gmail.com>
2023-04-27 13:55:51 +0700

    new

--- 01_face_dataset.py
+++ 01_face_dataset.py
@@ -12,12 +12,16 @@ Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
 import cv2
 import os
 
+
+
 cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video width
 cam.set(4, 480) # set video height
 
+
+
 #face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-face_detector= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_detector= cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 #face_detector.load('home/pi/haarcascade_frontalface_default.xml')
 
 # For each person, enter one numeric face id
@@ -30,9 +34,9 @@ count = 0
 while(True):
 
     ret, img = cam.read()
-    img = cv2.flip(img, 1) # flip video image vertically
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    img2 = cv2.flip(img, 1) # flip video image vertically
+    gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
     for (x,y,w,h) in faces:
 
@@ -47,12 +51,13 @@ while(True):
     k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
     if k == 27:
         break
-    elif count >= 30: # Take 30 face sample and stop video
+    elif count >= 50: # Take 50 face sample and stop video
          break
 
 # Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
 cam.release()
 cv2.destroyAllWindows()
+
 
 
