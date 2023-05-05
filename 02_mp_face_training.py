d7a1e67f75ee2d5b024f679cddd9efe3b8c1db51
alvianavivi <alvianagithub@gmail.com>
2023-05-06 01:15:36 +0700

    update

--- 02_mp_face_training.py
+++ 02_mp_face_training.py
@@ -15,6 +15,9 @@ recognizer = cv2.face.LBPHFaceRecognizer_create()
 mp_face_detection = mp.solutions.face_detection
 face_detector = mp.solutions.face_detection.FaceDetection(model_selection=1,min_detection_confidence=0.5)
 face_mesh = mp.solutions.face_mesh.FaceMesh()
+mp_drawing = mp.solutions.drawing_utils
+
+mp_face_mesh = mp.solutions.face_mesh
 
 # function to get the images and label data
 def getImagesAndLabels(path):
@@ -22,32 +25,48 @@ def getImagesAndLabels(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
     faceSamples=[]
     ids = []
+    face_encodings_dict = {}
 
     for imagePath in imagePaths:
 
-        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
-        img_numpy = np.array(PIL_img,'uint8')
+        #PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+        #img_numpy = np.array(PIL_img,'uint8')
 
-        #image = cv2.imread(imagePath)
-        #image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results_detection = face_detector.process(image_numpy)
+        image = cv2.imread(imagePath)
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        results_detection = face_detector.process(image_gray)
         detections = results_detection.detections
 
         id = int(os.path.split(imagePath)[-1].split(".")[1])
         #faces = detector.process(img_numpy)
 
-        for detection in faces.detections:
-            bbox = detection.location_data.relative_bounding_box
-            x, y, w, h = int(bbox.xmin * width), int(bbox.ymin * height), int(bbox.width * width), int(bbox.height * height)
+        for detection in detections:
+            bbox = mp_drawing._normalized_to_absolute_bounding_box(detection.location_data.relative_bounding_box, image.shape[1], image.shape[0])
+            face = image_gray[int(bbox[1]):int(bbox[1]+bbox[3]), int(bbox[0]):int(bbox[0]+bbox[2])]
             
-            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            # Extract the facial landmarks using the Face Mesh model in MediaPipe
+            with mp_face_mesh.FaceMesh(max_num_faces=1) as face_mesh:
+                results_mesh = face_mesh.process(face)
+                face_landmarks = results_mesh.multi_face_landmarks[0]
+            # Convert the landmarks to a numpy array and add it to the list of face encodings for this person
+            face_encoding = np.array([(landmark.x, landmark.y) for landmark in face_landmarks.landmark]).flatten()
+            if id in face_encodings_dict:
+                face_encodings_dict[id].append(face_encoding)
+            else:
+                face_encodings_dict[id] = [face_encoding] 
+
             ids.append(id)
 
-    return faceSamples,ids
+    return face_encodings_dict
 
 print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
-faces,ids = getImagesAndLabels(path)
-recognizer.train(faces, np.array(ids))
+faces = getImagesAndLabels(path)
+face_encodings = []
+labels = []
+for person_name, person_face_encodings in faces.items():
+    face_encodings.extend(person_face_encodings)
+    labels.extend([person_name]*len(person_face_encodings))
+recognizer.train(face_encodings, np.array(labels))
 
 # Save the model into trainer/trainer.yml
 recognizer.write('/home/pi/FaceRecog/trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
--- mediapipe_facerecog.py
+++ mediapipe_facerecog.py
@@ -1,33 +1,50 @@
-import cv2
 import mediapipe as mp
-mp_face_detection = mp.solutions.face_detection
-mp_drawing = mp.solutions.drawing_utils
+import cv2
+import numpy as np
+import uuid
 
-# For webcam input:
+face_detection = mp.solutions.face_detection.FaceDetection()
+landmark_detection = mp.solutions.face_mesh.FaceMesh()
+
 cap = cv2.VideoCapture(0)
-with mp_face_detection.FaceDetection(
-    model_selection=0, min_detection_confidence=0.5) as face_detection:
-  while cap.isOpened():
+cap.set(3, 640) # set video width
+cap.set(4, 480) # set video height
+
+while(True):
     success, image = cap.read()
     if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+        break
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = face_detection.process(image)
+    # Convert image to RGB format and detect faces
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = face_detection.process(image_rgb)
 
-    # Draw the face detection annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # Loop through each detected face and detect landmarks
     if results.detections:
-      for detection in results.detections:
-        mp_drawing.draw_detection(image, detection)
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Face Detection', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+        for detection in results.detections:
+            bbox = detection.location_data.relative_bounding_box
+            h, w, _ = image.shape
+            x1, y1, x2, y2 = int(bbox.xmin * w), int(bbox.ymin * h), int((bbox.xmin + bbox.width) * w), int((bbox.ymin + bbox.height) * h)
+            face_image = image[y1:y2, x1:x2]
+            landmarks = landmark_detection.process(cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB)).multi_face_landmarks[0]
+            
+            # Save the recognized face image to local storage
+            cv2.imwrite('face_{}.jpg'.format(str(uuid.uuid4())), face_image)
+            
+            # Draw landmarks on the original image
+            for lm in landmarks.landmark:
+                x, y = int(lm.x * face_image.shape[1]), int(lm.y * face_image.shape[0])
+                cv2.circle(face_image, (x, y), 2, (0, 255, 0), -1)
+                cv2.putText(face_image, str(lm.z), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+
+            # Draw bounding box around the face
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    # Display the output video
+    cv2.imshow('image', image)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
 cap.release()
+cv2.destroyAllWindows()
+
